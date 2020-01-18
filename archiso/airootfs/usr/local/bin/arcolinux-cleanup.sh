12860a09c6e906cbeac6ae1d72a21adbaa81173e
Erik Dubois <erik.dubois@gmail.com>
2020-01-18 18:37:22 +0100

    making sure everything is online

--- archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
+++ archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #fix userid 972 error lightdm
 #chage -E -1 lightdm
-rm -f /etc/systemd/system/display-manager.service
 rm -f /etc/sudoers.d/g_wheel
 rm -rf /usr/share/backgrounds/xfce
 rm -f /etc/polkit-1/rules.d/49-nopasswd_global.rules
@@ -9,6 +8,7 @@ rm -r /etc/systemd/system/etc-pacman.d-gnupg.mount
 rm /root/{.automated_script.sh,.zlogin}
 rm /etc/mkinitcpio-archiso.conf
 rm -r /etc/initcpio
+rm -f /etc/systemd/system/display-manager.service
 rm -rf /etc/lightdm
 #cleanup xfce
 for i in `ls /home/`; do rm -rf /home/$i/.config/* || exit 0; done
