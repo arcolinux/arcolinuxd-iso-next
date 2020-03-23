03f85dd03dd43ef1f28519701d216171acd6e615
Erik Dubois <erik.dubois@gmail.com>
2020-03-23 08:51:50 +0100

    bumpnr calamares themes

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinuxd-next
-iso_label="arcolinuxd-next-v20.4.5"
+iso_label="arcolinuxd-next-v20.4.6"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinuxD Live/Rescue CD"
-iso_version="v20.4.5"
+iso_version="v20.4.6"
 install_dir=arch
 work_dir=work
 out_dir=out
