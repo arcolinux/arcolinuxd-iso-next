67d83d84d19cdd113fdd0d7092a06227ad4683f9
Erik Dubois <erik.dubois@gmail.com>
2020-05-21 11:40:46 +0200

    update check update-mirrors

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinuxd-next
-iso_label="arcolinuxd-next-v20.6.2"
+iso_label="arcolinuxd-next-v20.6.3"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinuxD Live/Rescue CD"
-iso_version="v20.6.2"
+iso_version="v20.6.3"
 install_dir=arch
 work_dir=work
 out_dir=out
